cf019074c96aa1be6d2cf6cc49660a40d1bf3542
ibn0al0ghrawbi <75629323+ibn0al0ghrawbi@users.noreply.github.com>
2022-09-21 12:26:38 +0200

    Add files via upload

---
+++ hackathon/src/App.jsx
@@ -0,0 +1,30 @@
+import Hackathon from "./Pages/Hackathon";
+import "./App.css";
+import "./index.css";
+import { Routes, Route, Link } from "react-router-dom";
+import Tables from "./Pages/Tables_Admin";
+
+function App() {
+  return (
+    <div className="App">
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Hackathon</Link>
+          </li>
+          <li>
+            You can create, edit and delete the Hackathons{" "}
+            <Link to="/tables">here</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Hackathon />}></Route>
+        <Route path="/tables" element={<Tables />}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
